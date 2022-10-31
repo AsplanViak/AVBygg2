757408515b5b59ecac3c1036c45ef00f8b74dd31
thomvill <thomas.villanger@asplanviak.no>
2022-10-31 14:59:08 +0100

    Update malknapp_script.py

--- Testfil.tab/TestSone.panel/Malknapp.pushbutton/malknapp_script.py
+++ Testfil.tab/TestSone.panel/Malknapp.pushbutton/malknapp_script.py
@@ -11,6 +11,15 @@ outwindu = script.get_output()
 # revitinstans = _HostApplication(__revit__)
 dokument = revit.doc
 
+# Kan sløyfes
+__cleanengine__ = True  # Dette forteller tolkeren at den skal sette opp en ny ironpython motor for denne knappen, slik at den ikke kommer i konflikt med andre funksjoner, settes nesten alltid til FALSE, TRUE når du jobber med knappen.
+__fullframeengine__ = False  # Denne er nødvendig for å få tilgang til noen moduler, denne gjør knappen vesentrlig tregere i oppstart hvis den står som TRUE
+__context__ = "zerodoc"  # Denne forteller tolkeren at knappen skal kunne brukes selv om et Revit dokument ikke er åpent.
+__helpurl__ = "google.no"  # Hjelp URL når bruker trykker F1 over knapp.
+__min_revit_ver__ = 2010  # knapp deaktivert hvis klient bruker lavere versjon
+__max_revit_ver__ = 2032  # Skjønner?
+__beta__ = False  # Knapp deaktivert hos brukere som ikke har spesifikt aktivert betaknapper
+
 def who_created_selection():
     selection = revit.get_selection()
     if revit.doc.IsWorkshared:
